61405936d003e6ae556d0d9f4d97989708d12511
ppcrong <ppcrong@gmail.com>
2018-03-01 18:22:31 +0800

    [ppcrong.20180301][UTILS] Add isValidEmail

--- src/main/java/com/ppcrong/utils/MiscUtils.java
+++ src/main/java/com/ppcrong/utils/MiscUtils.java
@@ -389,4 +389,14 @@ public class MiscUtils {
         return json;
     }
     // endregion [Asset]
+
+    // region [Check]
+    public static boolean isValidEmail(String email) {
+
+        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+        java.util.regex.Matcher m = p.matcher(email);
+        return m.matches();
+    }
+    // endregion [Check]
 }
